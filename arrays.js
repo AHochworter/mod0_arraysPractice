19f4277fcb6f0ec250e13d248b76ab2ffe12d50d
Ann Hochworter <ann.hochworter@gmail.com>
2023-06-07 13:58:39 -0600

    Add values to arrays for strings, numbers, and boolean

---
+++ arrays.js
@@ -0,0 +1,21 @@
+// •	Declare a variable that stores an Array of at least 4 Strings.
+var favRestaurants = ["Biaggis", "Qdoba", "PF Changs", "Raffs Tavern"]
+
+// •	Declare a variable that stores an Array of at least 4 Numbers.
+var ageFriends = [80, 48, 61, 56, 49]
+
+// •	Declare a variable that stores an Array of at least 4 Booleans.
+var arrBooleans = [true, true, false, false, false]
+
+// •	Commit your work.
+
+// •	Call a different Array method on each of the 3 arrays you created above. On the line of code above each method call, write (in a JavaScript comment) an explanation in plain English as to what impact calling that method will have on that specific array.
+// •	Commit your work.
+// •	[Intentionally open-ended] Demonstrate your understanding of index positions in this file. You can write an explanation, provide some examples with the Arrays you’ve created, or anything else.
+// •	Commit your work.
+// Create another file in the same directory, name it self-evaluation.md. In Markdown, answer the following questions:
+// •	How confident do you feel with the content on Arrays so far?
+// •	Is there any additional learning you need or want to do before moving to the next lesson?
+// •	What questions do you have about Arrays?
+// •	Commit your work.
+// After you’ve completed the tasks above, push your work up to a new GitHub repository. Provide the GitHub repository link in the submission form.
